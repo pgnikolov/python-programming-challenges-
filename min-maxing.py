eed7e1b19a97c509af8c2a5a82a1e90c5d3c769d
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-05-05 20:31:08 +0200

    Create min-maxing.py

---
+++ min-maxing.py
@@ -0,0 +1,13 @@
+# Define a function named largest_difference that takes a list of numbers as its only parameter.
+# Your function should compute and return the difference between the largest and smallest number in the list.
+# For example, the call largest_difference([1, 2, 3]) should return 2 because 3 - 1 is 2.
+# You may assume that no numbers are smaller or larger than -100 and 100.
+
+# time - 3 mins
+def largest_difference(numbers):
+    return max(numbers) - min(numbers)
+
+
+numbers_list = [1, 2, 3]
+result = largest_difference(numbers_list)
+print(result)
