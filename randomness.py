8322016fa8b7882e68742bfb8a1fdc0aa35261b0
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-05-05 17:25:03 +0200

    Create randomness.py

---
+++ randomness.py
@@ -0,0 +1,13 @@
+# Define a function, random_number, that takes no parameters.
+# The function must generate a random integer between 1 and 100, both inclusive, and return it.
+# Calling the function multiple times should (usually) return different numbers.
+# For example, calling random_number() some times might first return 42, then 63, then 1.
+
+# time: 25 min
+# code time 1 min
+import random
+
+
+def random_number():
+    return random.randint(1, 100)
+
