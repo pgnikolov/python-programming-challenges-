cb0918979e8f10c0a0409dd71c4b91361b2a7c7c
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-05-06 14:22:14 +0200

    Create divisible-by-3.py

---
+++ divisible-by-3.py
@@ -0,0 +1,9 @@
+# Define a function named div_3 that returns True if its single integer parameter is divisible by 3 and False otherwise.
+# For example, div_3(6) is True because 6/3 does not leave any remainder.
+# However div_3(5) is False because 5/3 leaves 2 as a remainder.
+
+# time 1 min
+def div_3(n):
+    return n % 3 == 0
+
+
