7003cda7ef0852cca102c51a63243bf9172a7274
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-05-06 16:34:24 +0200

    Create writing-short-code.py

---
+++ writing-short-code.py
@@ -0,0 +1,13 @@
+# Define a function named convert that takes a list of numbers as its only parameter
+# and returns a list of each number converted to a string.
+# For example, the call convert([1, 2, 3]) should return ["1", "2", "3"].
+# What makes this tricky is that your function body must only contain a single line of code.
+
+# time 1 min
+def convert(nums):
+    return [str(char) for char in nums]
+
+
+nums = [1, 2, 4, 5]
+print(convert(nums)
+      
