ba93d306606454ce6cbbc4aab96483de7f5a98be
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-05-06 16:30:14 +0200

    Create boolean-and.py

---
+++ boolean-and.py
@@ -0,0 +1,8 @@
+# Define a function named triple_and that takes three parameters and
+# returns True only if they are all True and False otherwise.
+
+# time 12 mins
+
+def triple_and(p1, p2, p3):
+    return p1 and p2 and p3
+
