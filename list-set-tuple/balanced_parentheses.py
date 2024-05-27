634e444c539452d2b6453ba39961d99c7ff08f5d
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-05-27 07:10:56 +0200

    Create balanced_parentheses.py

---
+++ list-set-tuple/balanced_parentheses.py
@@ -0,0 +1,18 @@
+from collections import deque
+pairs = ['()', '{}', '[]']
+parentheses = deque(i for i in input())
+counter = len(parentheses) / 2
+
+while counter > 0:
+    firt = parentheses[0]
+    last = parentheses[-1]
+    counter -= 1
+    if firt + last in pairs:
+        parentheses.pop()
+        parentheses.popleft()
+
+if len(parentheses) == 0:
+    print("YES")
+else:
+    print("NO")
+
