5a7a69e5b773c970a72bddff03c489e3b1fbaf98
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-06-03 17:31:27 +0200

    Update fashion_boutique.py

--- list-set-tuple/fashion_boutique.py
+++ list-set-tuple/fashion_boutique.py
@@ -1,13 +1,28 @@
-clothes = [int(x) for x in input().split()]
+# clothes = [int(x) for x in input().split()]
+# rack_capacity = int(input())
+# new_rack_capacity = rack_capacity
+# racks_used = 1
+#
+# for _ in range(len(clothes)):
+#     clothe = clothes.pop()
+#     if new_rack_capacity - clothe < 0:
+#         racks_used += 1
+#         new_rack_capacity = rack_capacity
+#     new_rack_capacity -= clothe
+#
+# print(racks_used)
+
+box_clothes = [int(num) for num in input().split()]
+
 rack_capacity = int(input())
-new_rack_capacity = rack_capacity
-racks_used = 1
+total_racks = 1
+current_rack = []
 
-for _ in range(len(clothes)):
-    clothe = clothes.pop()
-    if new_rack_capacity - clothe < 0:
-        racks_used += 1
-        new_rack_capacity = rack_capacity
-    new_rack_capacity -= clothe
+while box_clothes:
+	if sum(current_rack) + box_clothes[-1] <= rack_capacity:
+		current_rack.append(box_clothes.pop())
+	else:
+		current_rack.clear()
+		total_racks += 1
 
-print(racks_used)
+print(total_racks)
