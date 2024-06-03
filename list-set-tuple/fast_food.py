faae01010d26292fa710f7c08ed20ea7cb0a4cd7
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-06-03 16:43:17 +0200

    Update fast_food.py

--- list-set-tuple/fast_food.py
+++ list-set-tuple/fast_food.py
@@ -1,22 +1,39 @@
+# from collections import deque
+#
+# food = int(input())
+#
+# orders = deque([int(i) for i in input().split()])
+#
+# print(max(orders))
+#
+# while True:
+#     if len(orders) == 0:
+#         break
+#     if food < orders[0]:
+#         break
+#     else:
+#         food -= orders[0]
+#         orders.popleft()
+#
+# if len(orders) == 0:
+#     print('Orders complete')
+# else:
+#     orders = list(str(n) for n in orders)
+#     print(f'Orders left: {" ".join(orders)}')
+
 from collections import deque
+food_qnt = int(input())
 
-food = int(input())
-
-orders = deque([int(i) for i in input().split()])
-
-print(max(orders))
+orders_food = deque(int(num) for num in input().split())
+print(max(orders_food))
 
 while True:
-    if len(orders) == 0:
-        break
-    if food < orders[0]:
-        break
-    else:
-        food -= orders[0]
-        orders.popleft()
+	if orders_food and orders_food[0] <= food_qnt:
+		food_qnt -= orders_food.popleft()
+	else:
+		break
 
-if len(orders) == 0:
-    print('Orders complete')
+if orders_food:
+	print(f"Orders left: {' '.join(map(str, orders_food))}")
 else:
-    orders = list(str(n) for n in orders)
-    print(f'Orders left: {" ".join(orders)}')
+	print("Orders complete")
