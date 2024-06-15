aeef36e6a1990faef92817b73392ce228cb53f94
plamen <pgnikolov@gmail.com>
2024-06-15 20:15:38 +0200

    week24

--- HackerRank/week-24/capitalize.py
+++
@@ -1,6 +0,0 @@
-# Complete the solve function below.
-def solve(s):
-    words = s.split()
-    capitalized_words = [word.capitalize() if word[0].isalpha() else word for word in words]
-    return ' '.join(capitalized_words)
-
