366322326c75d0be4ba863bd43992eb8288c6a75
pgnikolov <pgnikolov@gmail.com>
2024-08-19 16:06:54 +0200

    leet

---
+++ online-challenges/LeetCode/jump-game.py
@@ -0,0 +1,11 @@
+def canJump(nums):
+    max_reachable = 0
+    n = len(nums)
+
+    for i in range(n):
+        if i > max_reachable:
+            return False
+        max_reachable = max(max_reachable, i + nums[i])
+        if max_reachable >= n - 1:
+            return True
+    return False
