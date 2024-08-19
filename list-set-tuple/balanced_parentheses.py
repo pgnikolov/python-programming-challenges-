fde66eba84d5ccb12935b3c3d4bfc19a606051ae
pgnikolov <pgnikolov@gmail.com>
2024-08-19 22:28:34 +0200

    test

--- list-set-tuple/balanced_parentheses.py
+++ list-set-tuple/balanced_parentheses.py
@@ -4,10 +4,10 @@ parentheses = deque(i for i in input())
 counter = len(parentheses) / 2
 
 while counter > 0:
-    firt = parentheses[0]
+    first = parentheses[0]
     last = parentheses[-1]
     counter -= 1
-    if firt + last in pairs:
+    if first + last in pairs:
         parentheses.pop()
         parentheses.popleft()
 
