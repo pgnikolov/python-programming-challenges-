a92dae7689ebc8d6c4ddb5a73964cbee81499799
pgnikolov <pgnikolov@gmail.com>
2024-08-23 17:35:50 +0300

    leetcode

---
+++ LeetCode/Top-Interview-150/find-substring.py
@@ -0,0 +1,56 @@
+def findSubstring(s, words):
+    if not s or not words:
+        return []
+
+    word_len = len(words[0])
+    num_words = len(words)
+    concat_len = word_len * num_words
+    word_count = {}
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+
+    result = []
+
+    for i in range(word_len):
+        left = i
+        right = i
+        current_window_count = {}
+
+        while right + word_len <= len(s):
+            word = s[right:right + word_len]
+            right += word_len
+
+            if word in word_count:
+                if word in current_window_count:
+                    current_window_count[word] += 1
+                else:
+                    current_window_count[word] = 1
+
+                while current_window_count[word] > word_count[word]:
+                    current_window_count[s[left:left + word_len]] -= 1
+                    left += word_len
+
+                if right - left == concat_len:
+                    result.append(left)
+            else:
+                current_window_count.clear()
+                left = right
+
+    return result
+
+
+# Example Usage:
+s = "barfoothefoobarman"
+words = ["foo", "bar"]
+print(findSubstring(s, words))  # Output: [0, 9]
+
+s = "wordgoodgoodgoodbestword"
+words = ["word", "good", "best", "word"]
+print(findSubstring(s, words))  # Output: []
+
+s = "barfoofoobarthefoobarman"
+words = ["bar", "foo", "the"]
+print(findSubstring(s, words))  # Output: [6, 9, 12]
