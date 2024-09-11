880e18c9b690b8778cfd7d324006d342f3a72022
pgnikolov <pgnikolov@gmail.com>
2024-09-11 05:14:59 +0200

    robotics

---
+++ LeetCode/Top-Interview-150/is-subsequence.py
@@ -0,0 +1,12 @@
+def isSubsequence(s: str, t: str) -> bool:
+    i, j = 0, 0
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+    return i == len(s)
+
+
+# Example usage:
+print(isSubsequence("abc", "ahbgdc"))  # Output: True
+print(isSubsequence("axc", "ahbgdc"))  # Output: False
