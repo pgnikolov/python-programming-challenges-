f6f6921e638ab10612e4ab876e8d3b6c274096e8
pgnikolov <pgnikolov@gmail.com>
2024-09-11 05:11:28 +0200

    stack

--- list-set-tuple/balanced_parentheses.py
+++ list-set-tuple/balanced_parentheses.py
@@ -1,19 +1,23 @@
-from collections import deque
+parentheses_input = input()
+stack = []
 
-pairs = ['()', '{}', '[]']
-parentheses = deque(i for i in input())
-counter = len(parentheses) / 2
-
-while counter > 0:
-    first = parentheses[0]
-    last = parentheses[-1]
-    counter -= 1
-    if first + last in pairs:
-        parentheses.pop()
-        parentheses.popleft()
-
-if len(parentheses) == 0:
-    print("YES")
+for symbol in parentheses_input:
+    if symbol in ['[', '{', '(']:
+        stack.append(symbol)
+    else:
+        if not stack:
+            print("NO")
+            break
+        if symbol == ']' and stack[-1] != '[':
+            print("NO")
+            break
+        elif symbol == '}' and stack[-1] != '{':
+            print("NO")
+            break
+        elif symbol == ')' and stack[-1] != '(':
+            print("NO")
+            break
+        else:
+            stack.pop()
 else:
-    print("NO")
-
+    print("YES")
