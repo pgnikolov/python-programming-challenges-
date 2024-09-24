ecc6fde70357c4f8660baf3d7ba9a4eb1df7b06c
pgnikolov <pgnikolov@gmail.com>
2024-09-25 00:20:25 +0200

    changes

--- LeetCode-75/merge-strings-alternately.py
+++ LeetCode-75/merge-strings-alternately.py
@@ -1,4 +1,19 @@
 def mergeAlternately(word1: str, word2: str) -> str:
+    """
+    Merges two strings alternately.
+
+    This function takes two strings, word1 and word2, and returns a new string
+    where characters from word1 and word2 are interleaved. If one string is longer
+    than the other, the remaining characters from the longer string are appended
+    at the end.
+
+    Parameters:
+    word1 (str): The first input string.
+    word2 (str): The second input string.
+
+    Returns:
+    str: The merged string.
+    """
 
     result = []
     i = 0
