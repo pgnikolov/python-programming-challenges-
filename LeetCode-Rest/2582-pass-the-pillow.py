ebba8e70d136336b4918a8596ccee2fb566b3602
pgnikolov <pgnikolov@gmail.com>
2024-09-25 00:50:45 +0200

    change

--- LeetCode-Rest/2582-pass-the-pillow.py
+++
@@ -1,2 +0,0 @@
-def passthepillow(self, n, time):
-    return n - abs((n - 1) - (time % (2 * (n - 1))))
--- LeetCode-Rest/three-consecutive-odds.py
+++
@@ -1,9 +0,0 @@
-def three_odds(arr: list) -> bool:
-    counter = 0
-    for num in arr:
-        if num % 2 != 0:
-            counter += 1
-        else:
-            counter = 0
-        if counter == 3:
-            return True
